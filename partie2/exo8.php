397b7ff07737a0c01729104876d7274285fd0805
Anthony <semprini.anthony@gmail.com>
2023-01-27 12:35:46 +0100

    affiche img repeter

---
+++ partie2/exo8.php
@@ -0,0 +1,11 @@
+<h1>Exercice</h1>
+
+<p>Soit l’URL suivante : http://my.mobirise.com/data/userpic/764.jpg
+Créer une fonction personnalisée permettant d’afficher l’image N fois à l’écran.
+Exemple :
+repeterImage($url,4);</p>
+
+<h2>Resulbtat</h2>
+
+<?php
+
